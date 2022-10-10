717bc83f89051e694d3c460d1e00ec4aba84385b
Denis Istomin <REX.HD.KANAL@GMAIL.COM>
2022-10-10 16:07:28 +0300

    adequate logger

--- ADService/logger.cpp
+++ ADService/logger.cpp
@@ -1,36 +1,14 @@
 #include "logger.h"
-ILogger::ILogger(bool ts, QObject *parent) : _timeStamp(ts), QObject(parent) {}
-void ILogger::log(const QString& msg) { emit logged(); }
-void ILogger::setTimeStamp(bool ts) { _timeStamp=ts; }
-bool ILogger::timeStamp() { return _timeStamp; }
-StrLog::StrLog(QString& strLog, bool timeStamp/* = true*/, QObject* parent/* = nullptr*/) :
-	_strLog(strLog),
-	ILogger(timeStamp, parent)
+Logger::Logger(bool timeStamp /*=true*/, QObject* parent/*=nullptr*/)
+	:_timeStamp(timeStamp)
+	, QObject(parent) {}
+QString Logger::getLog() const
 {
+	return _str;
 }
-StrLog::StrLog(bool timeStamp/* = true*/, QObject* parent/* = nullptr*/) :
-		ILogger(timeStamp, parent)
+void Logger::log(const QString& msg)
 {
+	_str.push_back((_timeStamp ?
+		QTime::currentTime().toString():"") + msg);
+	emit(logged(msg));
 }
-void StrLog::log(const QString& msg)
-{
-	_strLog.push_back((_timeStamp ? QTime::currentTime().toString()+ " ":"") + msg + "\n");
-	ILogger::log(msg);
-}
-QString StrLog::getLog() const { return _strLog; }
-
-//GlobalStrLog::GlobalStrLog(QString& strLog, bool timeStamp = true, QObject* parent = nullptr) :
-//	ILogger(parent)
-//{
-//	global::_globalStrLog    = strLog;
-//	global::_globalTimeStamp = timeStamp;
-//}
-//void GlobalStrLog::log(const QString& msg)
-//{
-//	global::_globalStrLog.push_back((global::_globalTimeStamp ? QTime::currentTime().toString() + " " : "") + msg + "\n");
-//	ILogger::log(msg);
-//}
-//QString GlobalStrLog::getLog()
-//{
-//	return global::_globalStrLog;
-//}
--- ADService/logger.h
+++ ADService/logger.h
@@ -1,66 +1,18 @@
 #pragma once
-
-#include <QObject>
-#include <QString>
-#include <QDateTime>
-//#ifndef GLOBAL_LOG
-//#define GLOBAL_LOG
-//	namespace global
-//	{
-//		QString _globalStrLog;
-//		bool _globalTimeStamp;
-//	}
-//#endif
-class ILogger : public QObject
+#include <qobject.h>
+#include <qdatetime.h>
+class Logger : public QObject
 {
 	Q_OBJECT
-protected:
 	bool _timeStamp;
+	QString _str;
 public:
-	ILogger(bool timeStamp,QObject *parent);
-	virtual void log(const QString&);
-	bool timeStamp();
-	void setTimeStamp(bool);
-signals:
-	void logged();
-
-};
-
-
-class StrLog : ILogger
-{
-	
-protected:
-	QString _strLog;
-public:
-	StrLog(bool timeStamp, QObject* parent = nullptr);
-	StrLog(QString& strLog, bool timeStap = true, QObject* parent = nullptr);
-	void log(const QString& msg) override;
+	explicit Logger(bool timeStamp = true, QObject* parent = nullptr);
 	QString getLog() const;
+public slots:
+	void log(const QString& msg);
+signals:
+	void logged(const QString& msg);
+
 };
 
-
-//class GlobalStrLog : ILogger
-//{
-//protected:
-//	GlobalStrLog(bool timeStamp, QObject* parent);
-//	GlobalStrLog(QString& strLog, bool timeStap, QObject* parent);
-//public:
-//
-//    void log(const QString& msg);
-//	static QString getLog();
-//};
-//
-//
-//template<class LogStrategy>
-//class Logger  : public LogStrategy
-//{
-//
-//public:
-//	Logger(QString logStr = "", bool ts = true, QObject* parent = nullptr) :
-//		LogStrategy(logStr,ts,parent)
-//	{}
-//	~Logger()
-//	{}
-//
-//};
