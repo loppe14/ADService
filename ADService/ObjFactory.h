a9fc300c2af6019e77597dedacb503164988b927
Denis Istomin <REX.HD.KANAL@GMAIL.COM>
2022-11-06 14:58:11 +0300

    variable template parameters at the objFactory

--- ADService/ObjFactory.h
+++ ADService/ObjFactory.h
@@ -1,12 +1,32 @@
 #pragma once
 #include <memory>
+
 template<class T>
+struct newPolicy
+{
+	template<typename ...Args>
+	static auto make(Args&&...args) ->decltype(new T(std::forward<Args>(args)...))	
+	{
+		return new T(std::forward<Args>(args)...);
+	}
+};
+template <class T>
+struct uniquePtrPolicy
+{
+	template<typename ...Args>
+	static auto make(Args&&...args) ->decltype(std::make_unique<T, Args...>(std::forward<Args>(args)...))
+	{
+		std::make_unique<T, Args...>(std::forward<Args>(args)...);
+	}
+};
+template<template<class T> class Policy>
 class ObjFactory
 {
 public: 
-	static auto create() ->decltype(std::make_unique<T>()) 
+	template<typename ...Args>
+	static auto create(Args&&...args) ->decltype(Policy::make(std::forward<Args>(args)...))
 	{
-		return std::make_unique<T>();
+		return Policy::make(std::forward<Args>(args)...);
 	}
 };
 
